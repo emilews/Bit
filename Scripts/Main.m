eda61f16aecccb145a05bf3c954f1d76488415d4
Emilio Wong <emilews.ew@gmail.com>
2019-05-05 19:12:26 -0700

    Dates now fixed

--- Scripts/Main.m
+++ Scripts/Main.m
@@ -1,4 +1,7 @@
+%Add path for the datasets
+addpath('DataSets')
 %First we create the zeroed arrays
+dates = {};
 vol = zeros(size(Volume));
 hi = zeros(size(High));
 lo = zeros(size(Low));
@@ -16,6 +19,15 @@ for i = 1 : tam;
     lo(i) = Low(tam2);
     op(i) = Open(tam2);
     cl(i) = Close(tam2);
+    dates{i,1} = Date{tam2};
     tam2 = tam2 - 1;
 end
-
+%We only have 3 months worth of data, so we look for the index where the
+%date is Feb 5 2019 (05/02/2019) and save the index
+start = 0;
+for i = 1 : 2198;
+    if(strcmp(dates{i,1}, '05/02/2019'))
+        start = i;
+        i = 2198;
+    end
+end
